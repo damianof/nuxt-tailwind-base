a52a3a70371955e3571d847204f6cf99defbdba6
damianof <damianof@users.noreply.github.com>
2023-12-05 17:20:06 -0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-4">
+    <!-- <NuxtWelcome /> -->
+    <span class="text-3xl text-blue-500">This is a test</span>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  }
+})
